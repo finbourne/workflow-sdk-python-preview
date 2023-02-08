89c1ce7a7c14a720a65cc91d0db8505c2e20da3d
Concourse <concourse@finbourne.com>
2023-02-08 15:06:27 +0000

    Update SDK 0.1.133 [publish]

--- sdk/lusid_workflow/api/__init__.py
+++ sdk/lusid_workflow/api/__init__.py
@@ -5,4 +5,3 @@ from __future__ import absolute_import
 # import apis into api package
 from lusid_workflow.api.task_definitions_api import TaskDefinitionsApi
 from lusid_workflow.api.task_instances_api import TaskInstancesApi
-from lusid_workflow.api.updates_api import UpdatesApi
