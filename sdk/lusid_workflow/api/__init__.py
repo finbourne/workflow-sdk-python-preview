9dbdb24ae4faa0ef8542266c6ddeaee5eda3aac2
Concourse <concourse@finbourne.com>
2024-04-10 12:57:36 +0000

    Update SDK 0.1.879 [publish]

--- sdk/lusid_workflow/api/__init__.py
+++ sdk/lusid_workflow/api/__init__.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from lusid_workflow.api.event_handlers_api import EventHandlersApi
 from lusid_workflow.api.task_definitions_api import TaskDefinitionsApi
 from lusid_workflow.api.tasks_api import TasksApi
 from lusid_workflow.api.workers_api import WorkersApi
